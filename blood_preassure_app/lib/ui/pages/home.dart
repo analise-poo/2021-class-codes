066fe97077b478c5e1a2aee38813031919c25210
Mario H. Adaniya <mhadaniya@gmail.com>
2021-04-12 22:21:48 -0300

    :memo: 12/04 - mexendo com o candle chart

--- blood_preassure_app/lib/ui/pages/home.dart
+++ blood_preassure_app/lib/ui/pages/home.dart
@@ -1,18 +1,43 @@
-import 'package:blood_preassure_app/utils/colors.dart';
 import 'package:charts_painter/chart.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_icons/flutter_icons.dart';
 
 import '../components/components.dart';
+import '../../utils/utils.dart';
 
-class HomePage extends StatelessWidget {
+class CandleItem {
+  CandleItem(this.min, this.max);
+
+  final double max;
+  final double min;
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CandleItem> _value = [
+    CandleItem(2, 10),
+    CandleItem(1, 5),
+    CandleItem(1, 7),
+    CandleItem(2, 4),
+    CandleItem(3, 9),
+    CandleItem(2, 10),
+  ];
+
+  int _selected;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: CircleAvatar(
-          backgroundColor: Colors.orange,
+          radius: 8,
+          backgroundImage: NetworkImage(
+            'https://uifaces.co/our-content/donated/KtCFjlD4.jpg',
+          ),
         ),
         actions: [
           IconButton(icon: Icon(FontAwesome5Solid.equals), onPressed: null)
@@ -49,14 +74,110 @@ class HomePage extends StatelessWidget {
               ),
             ),
             Container(
-              child: Chart(
-                state: ChartState.line(
-                  ChartData.fromList(
-                    <double>[1, 3, 4, 2, 7, 6, 2, 5, 4]
-                        .map((e) => BubbleValue<void>(e))
-                        .toList(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12, bottom: 10),
+                    child: Row(
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: AppColors.blue,
+                              radius: 5,
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              'Systolic',
+                              style: TextStyle(fontSize: 12),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.orange,
+                              radius: 5,
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              'Diastolic',
+                              style: TextStyle(fontSize: 12),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Icon(
+                          Icons.info,
+                          size: 14,
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 2.0,
+                    ),
+                    child: CandleChart<CandleItem>(
+                      data: _value,
+                      dataToValue: (CandleItem value) =>
+                          CandleValue(value.min, value.max),
+                      height: MediaQuery.of(context).size.height * .275,
+                      chartBehaviour: ChartBehaviour(
+                          isScrollable: true,
+                          onItemClicked: (item) {
+                            print('fui clicado!');
+                            setState(() {
+                              _selected = item;
+                            });
+                          }),
+                      backgroundDecorations: [
+                        HorizontalAxisDecoration(
+                          showValues: true,
+                          lineWidth: .5,
+                          dashArray: [2, 4],
+                          legendFontStyle: TextStyle(
+                            color: AppColors.blue,
+                            fontSize: 10,
+                          ),
+                          legendPosition: HorizontalLegendPosition.start,
+                        ),
+                      ],
+                      foregroundDecorations: [
+                        ValueDecoration(
+                          alignment: Alignment.topCenter,
+                          textStyle: TextStyle(
+                            color: AppColors.blue,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                      chartItemOptions: BarItemOptions(
+                        minBarWidth: 6.0,
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        color: Theme.of(context)
+                            .colorScheme
+                            .primary
+                            .withOpacity(1.0),
+                        radius: BorderRadius.all(
+                          Radius.circular(100.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
